a54b7bd2116f41a8ed6e8253c92d0e4cc0af5d23
mingzezh <mingzezh@users.noreply.github.com>
2025-11-15 11:43:56 -0800

    feat: word embeddings

---
+++ main.py
@@ -0,0 +1,51 @@
+from sentence_transformers import SentenceTransformer, util
+from dataclasses import dataclass
+
+
+@dataclass
+class Club:
+    name: str
+    labels: list[str]
+
+
+class Embedding:
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.cache = dict()
+
+    def vectorize(self, phrase: str):
+        index = phrase
+        if index in self.cache:
+            return self.cache[index]
+        vector = self.model.encode(phrase)
+        self.cache[index] = vector
+        return vector
+
+
+embedding = Embedding()
+
+
+def cos_sim(p1: str, p2: str) -> float:
+    v1 = embedding.vectorize(p1)
+    v2 = embedding.vectorize(p2)
+    return util.cos_sim(v1, v2)
+
+
+def score(club: Club, user_input: list[str]) -> float:
+    final_score = 0
+    for label in club.labels:
+        for phrase in user_input:
+            final_score += cos_sim(label, phrase)
+
+    return final_score
+
+
+def main():
+    club = Club("chinese club", ["chinese", "language", "liguistic"])
+    user_input = ["manderin"]
+
+    print(score(club, user_input))
+
+
+if __name__ == "__main__":
+    main()

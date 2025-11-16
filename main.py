cd0337ab6216a2f5063d381ef85e41843dfef84f
mingzezh <mingzezh@users.noreply.github.com>
2025-11-15 16:55:36 -0800

    feat: score request handler

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
-from model.club import Club
+from flask import Flask, render_template, request, jsonify
+from model.club import Club, read_club_data
 from model.score import score
+import json
 
 app = Flask(__name__, static_folder="web/static", template_folder="web/templates")
+
+club_list = read_club_data("data.json")
+
+
+@app.route("/scores", methods=["POST"])
+def scores():
+    data = json.loads(request.data.decode("utf-8"))
+    output = []
+    for club in club_list:
+        club_score = score(club, data)
+        output.append(
+            {
+                "name": club.name,
+                "score": club_score.item(),
+                "labels": club.labels,
+            }
+        )
+
+    return jsonify(output), 200
 
 
 @app.route("/")
--- model/club.py
+++ model/club.py
@@ -1,7 +1,20 @@
 from dataclasses import dataclass
+import json
 
 
 @dataclass
 class Club:
     name: str
     labels: list[str]
+
+
+def read_club_data(path: str) -> list[Club]:
+    with open(path) as f:
+        out = json.load(f)
+
+    club_list = []
+    for data in out:
+        club = Club(data["name"], data["labels"])
+        club_list.append(club)
+
+    return club_list
